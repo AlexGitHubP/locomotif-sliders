9d60f198086d07f3d729da3e546bc00f8b562e8d
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2019-10-26 15:46:08 +0300

    Update show.blade.php

--- src/views/show.blade.php
+++ src/views/show.blade.php
@@ -1,6 +1,14 @@
-<p>Slide details</p>
+@extends('admin::inc/header')
+@section('title', 'Testimonial detail')
+@include('admin::inc/menu')
 
-<p>Name is: {{ $slide->name }}</p>
-<p>status is: {{ $slide->status }}</p>
-
-<a href="/admin/sliders">Go back</a>
+@section('content')
+<div class="container">
+    <div class="cms-body">
+		<p>Slide details</p>
+		<p>Name is: {{ $slide->name }}</p>
+    <p>status is: {{ $slide->status }}</p>
+    <a href="/admin/sliders">Go back</a>
+	</div>
+</div>
+@endsection

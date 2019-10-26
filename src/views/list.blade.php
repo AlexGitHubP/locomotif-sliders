ab9513360471fab52d68b121747ce53aa67e0f7d
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2019-10-26 15:48:04 +0300

    Update list.blade.php

--- src/views/list.blade.php
+++ src/views/list.blade.php
@@ -1,7 +1,17 @@
-<p>this is sliders listing</p>
+@extends('admin::inc/header')
+@section('title', 'Testimonials list')
+@include('admin::inc/menu')
 
-@foreach($sliders as $slide)
-	<p>Slide: {{ $slide->name }} <a href="/admin/sliders/{{$slide->id}}/edit">Edit slide</a> <form action="/admin/sliders/{{ $slide->id }}" method="POST">@method('DELETE') @csrf <input type="submit" value="Delete slide"/></form></p>
-@endforeach
+@section('content')
+<div class="container">
+	<div class="cms-body">
+		<p>this is sliders listing</p>
+		<a href="/admin/sliders/create">Add new slide</a>
+		@foreach($sliders as $slide)
+			<p>Slide: {{ $slide->name }} <a href="/admin/sliders/{{$slide->id}}/edit">Edit slide</a> <form action="/admin/sliders/{{ $slide->id }}" method="POST">@method('DELETE') @csrf <input type="submit" value="Delete slide"/></form></p>
+		@endforeach
+    	</div>
+</div>
 
-<a href="/admin/sliders/create">Add new slide</a>
+@endsection
+

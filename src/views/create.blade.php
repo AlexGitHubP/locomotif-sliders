f60b69360f94389f6429cc89c5820d214441863c
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2019-10-26 15:50:03 +0300

    Update create.blade.php

--- src/views/create.blade.php
+++ src/views/create.blade.php
@@ -1,17 +1,24 @@
-<p>Create a new slider</p>
+@extends('admin::inc/header')
+@section('title', 'Add testimonial')
+@include('admin::inc/menu')
+@section('content')
+<div class="container">
+    <div class="cms-body">
+    <p>Create a new slider</p>
+	<form method="POST" action=/admin/sliders>
+		@csrf
 
+		<div class="field-group">
+			<label for="name">Slider name</label>
+			<input type="text" name="name" value="">
+		</div>
+		<div class="field-group">
+			<label for="status">Slider status</label>
+			<input type="checkbox" name="status" value="1">
+		</div>
 
-<form method="POST" action=/admin/sliders>
-	@csrf
-
-	<div class="field-group">
-		<label for="name">Slider name</label>
-		<input type="text" name="name" value="">
+		<input type="submit" value="Store">
+	</form>
 	</div>
-	<div class="field-group">
-		<label for="status">Slider status</label>
-		<input type="checkbox" name="status" value="1">
-	</div>
-	
-	<input type="submit" value="Store">
-</form>
+</div>
+@endsection
